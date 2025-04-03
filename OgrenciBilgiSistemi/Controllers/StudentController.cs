63d580bb0666317aedc6c95b30a495a3d10eccbc
Hüseyin M. <121802540+huseyinmehmetoglu@users.noreply.github.com>
2025-04-03 23:10:31 +0300

    basit bir öğrenci bilgi sistemi
    Bu projede, ASP.NET Core MVC kullanarak basit bir Öğrenci Bilgi Sistemi geliştirdim. Uygulama, öğrenci kayıtları üzerinde temel CRUD işlemlerini gerçekleştirmekte ve Entity Framework Core aracılığıyla SQL Server veritabanıyla entegre çalışmaktadır. Ayrıca, başlangıç verilerini eklemek için seed data kullanılmıştır.

---
+++ OgrenciBilgiSistemi/Controllers/StudentController.cs
@@ -0,0 +1,108 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OgrenciBilgiSistemi.Data;
+using OgrenciBilgiSistemi.Models;
+
+namespace OgrenciBilgiSistemi.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var students = await _context.Students.ToListAsync();
+            return View(students);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Students.Add(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            foreach (var key in ModelState.Keys)
+            {
+                foreach (var error in ModelState[key].Errors)
+                {
+                    System.Diagnostics.Debug.WriteLine($"{key}: {error.ErrorMessage}");
+
+                }
+
+            }
+            return View(student);
+        }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+                return NotFound();
+            return View(student);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Students.Update(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+
+
+            }
+            foreach (var key in ModelState.Keys)
+            {
+                foreach (var error in ModelState[key].Errors)
+                {
+                    System.Diagnostics.Debug.WriteLine($"{key}: {error.ErrorMessage}");
+                }
+            }
+            return View(student);
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+          var student = await _context.Students
+                .FirstOrDefaultAsync(s => s.Id==id);
+
+            if (student == null)
+            
+                return NotFound();
+
+            return View(student);
+            
+        }
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SilmeOnayi(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            if(student != null)
+            {
+                _context.Students.Remove(student);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ OgrenciBilgiSistemi/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace OgrenciBilgiSistemi.Models
+{
+    public class Student
+    {
+        public int Id { get;set; }
+
+        [Required(ErrorMessage ="Öğrenci adı ve soyadı zorunludur." )]
+        public string FullName { get;set; }
+
+        [Required(ErrorMessage="Öğrenci yaşı zorunludur.")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage ="Öğrenci bölümü zorunludur.")]
+        public string Department { get; set; }
+
+        [Required(ErrorMessage ="Öğrenci e-posta adresi zorunludur..")]
+        [EmailAddress(ErrorMessage="Geçerli bir e-posta adresi giriniz..")]
+        public string Email { get; set; }   
+    }
+}
